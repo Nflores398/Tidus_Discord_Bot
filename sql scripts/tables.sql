c285fd3401c4fcf9deb607e677f84d07792ce918
Noah Flores <70032405+Nflores398@users.noreply.github.com>
2023-08-02 01:14:00 -0500

    Add files via upload

---
+++ sql scripts/tables.sql
@@ -0,0 +1,13 @@
+-- ####################################################################
+-- # Basic CREATE TABLE statement
+-- # See https://www.ibm.com/docs/en/db2-for-zos/13?topic=statements-create-table for complete syntax.
+-- ####################################################################
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'SoccerSteve!sC001AndL1kesToGoToSch0ol?!?'
+USE TidusBot
+DROP TABLE Birthday;
+
+CREATE TABLE Birthday (
+  userId VARCHAR(18) PRIMARY KEY NOT NULL
+  , userName VARCHAR(35) NOT NULL
+  , birthDate VARCHAR(5) NOT NULL
+);
